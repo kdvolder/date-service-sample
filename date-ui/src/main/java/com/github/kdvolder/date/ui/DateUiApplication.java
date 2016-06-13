fc5ad3d3850deb0aeaa7e4354e9da5768ec1df24
Kris De Volder <kdevolder@pivotal.io>
2016-06-13 14:26:33 -0700

    Attempt to make up for RestTemplate bean going missing since 1.3.2

--- date-ui/src/main/java/com/github/kdvolder/date/ui/DateUiApplication.java
+++ date-ui/src/main/java/com/github/kdvolder/date/ui/DateUiApplication.java
@@ -2,15 +2,24 @@ package com.github.kdvolder.date.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableHystrix
 public class DateUiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DateUiApplication.class, args);
+    }
+    
+    @Bean
+    RestTemplate restTemplate() {
+    	return new RestTemplate();
     }
 }

9d270dc4202061cb6961e7dfe67671c7793cc737
Kris De Volder <kdevolder@pivotal.io>
2016-06-13 15:17:02 -0700

    The missing link... a single @LoadBalanced annotation

--- date-ui/src/main/java/com/github/kdvolder/date/ui/DateUiApplication.java
+++ date-ui/src/main/java/com/github/kdvolder/date/ui/DateUiApplication.java
@@ -3,9 +3,8 @@ package com.github.kdvolder.date.ui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,6 +18,7 @@ public class DateUiApplication {
     }
     
     @Bean
+    @LoadBalanced
     RestTemplate restTemplate() {
     	return new RestTemplate();
     }
